e11750ec2bdc2a2814bcd49c87b103959e294b12
Stefan Georg <stefan.georg@minglabs.com>
2016-04-21 17:54:57 +0700

    Added in models for admin

--- gro_api/recipes/admin.py
+++ gro_api/recipes/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from gro_api.recipes.models import Recipe, RecipeRun, SetPoint, ActuatorOverride
+
 admin.site.register(Recipe)
 admin.site.register(RecipeRun)
 admin.site.register(SetPoint)
